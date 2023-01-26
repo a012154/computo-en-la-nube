caad2516af1868657113f1ade508577b1df25cb2
a012154 <113671650+a012154@users.noreply.github.com>
2023-01-26 14:09:34 -0600

    first

---
+++ proyectosOMP/pruebaOMP/pruebaOMP/pruebaOMP.cpp
@@ -0,0 +1,42 @@
+// pruebaOMP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include "pch.h"
+#include <iostream>
+#include <omp.h>
+
+#define N 10000
+#define chunk 200
+#define mostrar 10
+
+void imprimeArreglo(float* d) {
+	for (int x = 0; x < mostrar; x++) {
+		std::cout << d[x] << " - ";
+	}
+	std::cout << std::endl;
+}
+
+int main() {
+	std::cout << "Sumando Arreglos en Paralelo!\n";
+	float a[N], b[N], c[N];
+	int i;
+
+	for (i = 0; i < N; i++) {
+		a[i] = i * 10;
+		b[i] = (i + 3) * 3.7;
+	}
+
+	int pedazos = chunk;
+
+	#pragma omp parallel for shared(a,b,c,pedazos) private(i) schedule(static,pedazos)
+	
+	for (i = 0; i < N; i++) {
+		c[i] = a[i] + b[i];
+	}
+
+	std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo a: " << std::endl;
+	imprimeArreglo(a);
+	std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo b: " << std::endl;
+	imprimeArreglo(b);
+	std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo c: " << std::endl;
+	imprimeArreglo(c);
+}
